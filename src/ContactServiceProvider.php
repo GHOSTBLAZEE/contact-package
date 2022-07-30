417f39cc4d1515253e3dfe33a27c2719efce9457
GHOSTBLAZEE <sksurajpra138@gmail.com>
2022-07-30 20:08:03 +0530

    final change

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -11,18 +11,18 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/views', 'contact');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/contact.php',
+            'contact'
+        );
         $this->publishes([
             __DIR__ . '/config/contact.php' => config_path('contact.php'),
             __DIR__ . '/views' => resource_path('views/vendor/contact'),
-            __DIR__ . '/Http' => app_path('Http/Controllers/'),
+            __DIR__ . '/Http/Controllers' => app_path('Http/Controllers/'),
         ]);
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__ . '/config/contact.php',
-            'contact'
-        );
     }
 }
